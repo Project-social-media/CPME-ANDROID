1abc4a41f27e08fe964b142422b3044a7eb9eea1
Alexandre MATHIEU <94917883+Nauwk07@users.noreply.github.com>
2022-12-08 23:33:09 +0100

    Commit 2

--- app/src/main/java/edu/intech/mediatech/ConnexionFragment.java
+++ app/src/main/java/edu/intech/mediatech/ConnexionFragment.java
@@ -7,12 +7,20 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.util.List;
+
 import edu.intech.mediatech.databinding.FragmentConnexionBinding;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -20,14 +28,7 @@ public class ConnexionFragment extends Fragment {
 
     FragmentConnexionBinding binding;
 
-    Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("http://localhost:3000/api")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
-
-    MyApi myApi = retrofit.create(MyApi.class);
-
-    public ConnexionFragment() {
+    public ConnexionFragment() throws IOException {
         // Required empty public constructor
     }
 
@@ -62,6 +63,4 @@ public class ConnexionFragment extends Fragment {
             Navigation.findNavController(v).navigate(R.id.action_connexionFragment_to_lostFragmentPassword);
         });
     }
-
-
 }
--- app/src/main/java/edu/intech/mediatech/MyApi.java
+++ app/src/main/java/edu/intech/mediatech/MyApi.java
@@ -5,12 +5,10 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface MyApi {
-    @GET("/users")
+    @GET("users")
     Call<List<User>> getUsers();
-
-    @POST("/users")
-    Call<User> createUser(@Body User user);
 }
