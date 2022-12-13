942667ab5a12bd92bf97673fc2fc081896182baf
flaco <73465532+flacoZbi@users.noreply.github.com>
2022-12-13 20:06:15 +0100

    Date dialog

--- app/src/main/java/edu/intech/mediatech/models/PostCreationFragment.java
+++ app/src/main/java/edu/intech/mediatech/models/PostCreationFragment.java
@@ -1,21 +1,32 @@
 package edu.intech.mediatech.models;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Objects;
 
 import edu.intech.mediatech.R;
 import edu.intech.mediatech.databinding.FragmentPostCreationBinding;
@@ -37,7 +48,7 @@ public class PostCreationFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentPostCreationBinding.inflate(inflater, container, false);
@@ -53,10 +64,10 @@ public class PostCreationFragment extends Fragment {
         });
 
         binding.fgtPostValidateBtn.setOnClickListener(v -> {
-            String body = binding.fgtPostcrtBodyInput.getText().toString();
+            String body = Objects.requireNonNull(binding.fgtPostcrtBodyInput.getText()).toString();
             String media = binding.fgtPostcrtMediaLink.getText().toString();
 
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", java.util.Locale.getDefault());
             Date date = null;
 
             try {
@@ -73,9 +84,45 @@ public class PostCreationFragment extends Fragment {
             Post post = new Post(body, Collections.singletonList(media), date, facebook, instagram, linkedin, twitter);
 
             binding.fgtPostcrtBodyInput.setText(post.toString());
+
+            binding.fgtBtnDateDialog.setOnClickListener(v1 -> {
+                Log.d("TAG", "onViewCreated: TESSSSSSSSSSSSSSSSSSSSSSSSSSSSSST");
+                DatePickerFragment datePickerFragment = new DatePickerFragment(binding.fgtPostcrtDate);
+                datePickerFragment.show(getChildFragmentManager(), "datePicker");
+
+            });
+
         });
 
 
+    }
+
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+        private final EditText et;
+
+        public DatePickerFragment(EditText et){
+            this.et = et;
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog dialog = new DatePickerDialog(getActivity(), this, year, month, day);
+            dialog.getDatePicker().setMaxDate(c.getTimeInMillis());
+            return  dialog;
+        }
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            Calendar c = Calendar.getInstance();
+            c.set(year, month, day);
+            et.setText(c.toString());
+        }
     }
 
 }
