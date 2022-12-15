39f54e6a81a733e95b906432e6e81c1b7dc693ac
Alexandre MATHIEU <94917883+Nauwk07@users.noreply.github.com>
2022-12-15 13:40:55 +0100

    Commit

---
+++ app/src/main/java/edu/intech/mediatech/models/fragments/EditionFragment.java
@@ -0,0 +1,38 @@
+package edu.intech.mediatech.models.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import edu.intech.mediatech.databinding.FragmentEditionBinding;
+
+public class EditionFragment extends Fragment {
+
+    FragmentEditionBinding binding;
+
+    public EditionFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentEditionBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
